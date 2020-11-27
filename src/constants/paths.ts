8a05dab37d244804a2911617effcecd0aa463794
James Teow <james.teow83@gmail.com>
2020-11-27 01:55:00 -0500

    For consistency.

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,3 +1,3 @@
 export const BASEURL = 'https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/';
 export const COMMUNITIES = 'communities';
-export const HOMES = 'homes'
+export const HOMES = 'homes';
